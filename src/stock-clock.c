a2f2ebbda9a0cf7041a5d409a550bcb053136117
Kareem Saleh <kareemsal@gmail.com>
2016-02-15 15:42:14 -0500

    Irrelevant now

--- src/stock-clock.c
+++ src/stock-clock.c
@@ -6,8 +6,7 @@ static GFont s_time_font, s_stock_font, s_weather_font;
 Layer *s_up_arrow_layer = NULL, *s_down_arrow_layer = NULL;
 TextLayer *s_time_layer = NULL, *s_stock_layer = NULL, *s_weather_layer = NULL;
 
-//@todo Date layer and bitmap layer for date style.
-//@todo create pointer to be sent as context to all message events?
+//TODO: Date layer and bitmap layer for date style.
 
 /**
  * Updates the text layer with current time.
